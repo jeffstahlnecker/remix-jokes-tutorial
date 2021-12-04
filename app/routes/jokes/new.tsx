3d099d3c8d6eba21d6e095be66720135431b0294
jeffstahlnecker <45363541+jeffstahlnecker@users.noreply.github.com>
2021-12-04 05:55:50 -0800

    add joke route, new joke route

---
+++ app/routes/jokes/new.tsx
@@ -0,0 +1,24 @@
+export default function NewJokeRoute() {
+  return (
+    <div>
+      <form method="post">
+        <div>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content
+            <input type="text" name="content" />
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
